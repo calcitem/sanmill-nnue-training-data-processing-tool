c5733e968c83c2a332bacdc8c0e5bfc4a30923a5
Calcitem <calcitem@outlook.com>
2022-12-11 13:08:06 +0800

    改为逐行比较

--- sanmill-nnue-training-data-processing-tool/main.c
+++ sanmill-nnue-training-data-processing-tool/main.c
@@ -15,13 +15,16 @@ int main(void)
     FILE *in, *out;
 
     // 定义行指针
-    static char* lines;
+    static char** lines;
 
     // 定义行数
     int numLines = 0;
 
     // 为所有行分配内存
-    lines = (char*)malloc(MAX_LINES * MAX_LINE_LENGTH);
+    lines = (char**)malloc(MAX_LINES * sizeof(char*));
+    for (int i = 0; i < MAX_LINES; i++) {
+        lines[i] = (char*)malloc(MAX_LINE_LENGTH);
+    }
 
     // 打开输入文件
     if (fopen_s(&in, "all.txt", "r") != 0) {
@@ -40,26 +43,39 @@ int main(void)
         // 去掉换行符
         buffer[strcspn(buffer, "\r\n")] = '\0';
 
+        // 判断当前行是否重复
+        int isDuplicate = 0;
+        for (int i = 0; i < numLines; i++) {
+            // 如果当前行和之前的行相同，则设置标记
+            if (strcmp(buffer, lines[i]) == 0) {
+                isDuplicate = 1;
+                break;
+            }
+        }
+
         // 如果当前行是空行，或者与之前的行不同，则将其写入行指针
-        if (buffer[0] == '\0' || strcmp(buffer, lines + numLines * MAX_LINE_LENGTH) != 0) {
+        if (buffer[0] == '\0' || !isDuplicate) {
             // 如果行指针指向的内存已满，则退出循环
             if (numLines >= MAX_LINES) {
                 break;
             }
 
             // 将当前行存储到行指针
-            strcpy_s(lines + numLines * MAX_LINE_LENGTH, MAX_LINE_LENGTH, buffer);
+            strcpy_s(lines[numLines], MAX_LINE_LENGTH, buffer);
             numLines++;
         }
     }
 
     // 将行指针指向的内存中的每一行写入输出文件
     for (int i = 0; i < numLines; i++) {
-        fputs(lines + i * MAX_LINE_LENGTH, out);
+        fputs(lines[i], out);
         fputc('\n', out);
     }
 
     // 释放内存
+    for (int i = 0; i < MAX_LINES; i++) {
+        free(lines[i]);
+    }
     free(lines);
 
     // 关闭文件
